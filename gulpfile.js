404d03263356b4de12f148415c745f6774538635
Ashraf Ben Slimane <benslimaneashraf@gmail.com>
2018-01-12 18:38:56 +0100

    concat javascript files

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat');
+
+var jsSources = [
+  'components/scripts/script.js'
+];
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
+});
