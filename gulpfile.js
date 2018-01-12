e116ec1bdc4b87f145fe9de25577ad8b4503acf4
Ashraf Ben Slimane <benslimaneashraf@gmail.com>
2018-01-12 19:07:41 +0100

    add compass process

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    compass = require('gulp-compass');
 
 var jsSources = [
   'components/scripts/script.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }))
+    .pipe(gulp.dest('builds/development/css'))
 });
