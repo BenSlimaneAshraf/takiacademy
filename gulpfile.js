4ef074633bbd33bf087e5df9d225a726b5d08d08
Ashraf Ben Slimane <benslimaneashraf@gmail.com>
2018-01-14 01:41:05 +0100

    minified our html files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     compass = require('gulp-compass'),
     gulpif = require('gulp-if'),
+    minifyHTML = require('gulp-minify-html'),
     uglify = require('gulp-uglify');
 
 var env,
@@ -50,7 +51,7 @@ gulp.task('compass', function() {
 gulp.task('watch', function() {
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch(htmlSources, ['html']);
+  gulp.watch('builds/development/*.html', ['html']);
 });
 
 gulp.task('connect', function() {
@@ -61,7 +62,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
+  gulp.src('builds/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
     .pipe(connect.reload())
 });
 
