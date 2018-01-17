530a163c19a66d5a6a2a823c1c942b1d4bd420a6
Ashraf Ben Slimane <benslimaneashraf@gmail.com>
2018-01-17 13:53:29 +0100

    add aside to main

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -2,12 +2,20 @@
 var jQuery = require('jquery');
 
 jQuery(document).ready(function(){
-    jQuery('#svgfile1').load('../images/inbox.svg');
-    jQuery('#svgfile2').load('../images/notification.svg');
-    jQuery('#svgfile3').load('../images/search.svg');
-    jQuery('#svgfile4').load('../images/transactions.svg');
-    jQuery('#svgfile5').load('../images/boy.svg');
-    jQuery('#svgfile6').load('../images/logout.svg');
+    jQuery('#inbox').load('../images/nav/inbox.svg');
+    jQuery('#notification').load('../images/nav/notification.svg');
+    jQuery('#search').load('../images/nav/search.svg');
+    jQuery('#transactions').load('../images/nav/transactions.svg');
+    jQuery('#boy').load('../images/nav/boy.svg');
+    jQuery('#girl').load('../images/nav/girl.svg');
+    jQuery('#logout').load('../images/nav/logout.svg');
+
+    jQuery('#facebook').load('../images/social/facebook.svg');
+    jQuery('#twitter').load('../images/social/twitter.svg');
+    jQuery('#youtube').load('../images/social/youtube.svg');
+    jQuery('#instagram').load('../images/social/instagram.svg');
+    jQuery('#google-plus').load('../images/social/google-plus.svg');
+    jQuery('#sharethis').load('../images/social/sharethis.svg');
 });
 
 },{"jquery":2}],2:[function(require,module,exports){
