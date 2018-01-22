f0106293835f95787a6f873de6fc65f6bcd158ab
Ashraf Ben Slimane <benslimaneashraf@gmail.com>
2018-01-22 18:36:59 +0100

    add subheader to mobile view

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -2,13 +2,13 @@
 var jQuery = require('jquery');
 
 jQuery(document).ready(function(){
-    jQuery('#inbox').load('../images/nav/inbox.svg');
-    jQuery('#notification').load('../images/nav/notification.svg');
-    jQuery('#search').load('../images/nav/search.svg');
-    jQuery('#transactions').load('../images/nav/transactions.svg');
-    jQuery('#boy').load('../images/nav/boy.svg');
-    jQuery('#girl').load('../images/nav/girl.svg');
-    jQuery('#logout').load('../images/nav/logout.svg');
+    jQuery('.inbox').load('../images/nav/inbox.svg');
+    jQuery('.notification').load('../images/nav/notification.svg');
+    jQuery('.search').load('../images/nav/search.svg');
+    jQuery('.transactions').load('../images/nav/transactions.svg');
+    jQuery('.boy').load('../images/nav/boy.svg');
+    jQuery('.girl').load('../images/nav/girl.svg');
+    jQuery('.logout').load('../images/nav/logout.svg');
 
     jQuery('#facebook').load('../images/social/facebook.svg');
     jQuery('#twitter').load('../images/social/twitter.svg');
